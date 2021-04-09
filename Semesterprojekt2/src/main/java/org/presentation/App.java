4fde690a4cc098300c563933052fe6f2c41ff3ac
Karl-Emil Pantzar <kapan20@student.sdu.dk>
2021-04-09 12:43:03 +0200

    Updated GUI
    Skelett opdateret

--- Semesterprojekt2/src/main/java/org/presentation/App.java
+++ Semesterprojekt2/src/main/java/org/presentation/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 800, 600);
+        scene = new Scene(loadFXML("primary"), 1200, 900);
         stage.setScene(scene);
         stage.show();
     }
--- Semesterprojekt2/src/main/java/org/presentation/PrimaryController.java
+++ Semesterprojekt2/src/main/java/org/presentation/PrimaryController.java
@@ -1,6 +1,8 @@
 package org.presentation;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class PrimaryController {
@@ -8,5 +10,15 @@ public class PrimaryController {
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+
+    @FXML
+    private void switchToProducer() throws IOException {
+        App.setRoot("producer");
+    }
+
+    @FXML
+    private void switchToViewer() throws IOException {
+        App.setRoot("viewer");
     }
 }
