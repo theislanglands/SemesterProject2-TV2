c07ef8007fce8c435a665d2b535079ab99633a45
Theis Langlands <theislanglands@gmail.com>
2021-05-03 16:42:39 +0200

    Opstart af persistens a la DM07

--- Semesterprojekt2/src/main/java/Interfaces/DataLayerInterface.java
+++ Semesterprojekt2/src/main/java/Interfaces/DataLayerInterface.java
@@ -6,12 +6,14 @@ import java.util.List;
 
 public interface DataLayerInterface {
 
+    // flg metoder defineres for hver klasse!
     void saveProduction(Production prod);
-
     List<Production> getProductions();
+    public Production getProduction(int id);
+
 
     // returnerer true hvis lykkedes
     boolean updateProduction(String productionID, Production replaceProduction);
 
-    void createTestProductions();
+    // void createTestProductions();
 }
--- Semesterprojekt2/src/main/java/data/DataFacade.java
+++ Semesterprojekt2/src/main/java/data/DataFacade.java
@@ -4,96 +4,46 @@ import Interfaces.DataLayerInterface;
 import domain.Production;
 
 import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class DataFacade  {
-/*
-    String fileName = "src/main/resources/Persistence/productions.tvc";
-    File file = new File(fileName);
+public class DataFacade  { // add implements DataLayerInterface
 
-    @Override
-    public void saveProduction(Production prod) {
+    private static DataFacade instance;
+    private String url = "localhost";
+    private int port = 5432;
+    private String databaseName = "";
+    private String username = "";
+    private String password = "";
+    private Connection connection = null;
 
-    // open file
+    private DataFacade(){
+        initializePostgresqlDatabase();
+    }
 
-
-
-        try (ObjectOutputStream outputStream = new ObjectOutputStream(
-                new FileOutputStream(fileName, true)) {
-            @Override
-            protected void writeStreamHeader() throws IOException {
-                if (file.exists()) {
-                } else
-                    super.writeStreamHeader();
-            }
-        }) {
-            outputStream.writeObject(prod);
-            outputStream.close();
-        } catch(FileNotFoundException e) {
-            System.out.println("file not found");
-        } catch(IOException e) {
-            System.out.println("IOException");
+    public static DataFacade getInstance(){
+        if (instance == null) {
+            instance = new DataFacade();
         }
+        return instance;
+    }
 
+    private void initializePostgresqlDatabase() {
+        try {
+            DriverManager.registerDriver(new org.postgresql.Driver());
+            connection = DriverManager.getConnection("jdbc:postgresql://" + url + ":" + port + "/" + databaseName, username, password);
+        } catch (SQLException | IllegalArgumentException ex) {
+            ex.printStackTrace(System.err);
+        } finally {
+            if (connection == null) System.exit(-1);
+        }
     }
 
 
-
-    @Override
-    public List<Production> getProductions() {
-        Production readOne;
-        List<Production> readProductions = new ArrayList<>();
-
-        try (ObjectInputStream inputStream = new ObjectInputStream(
-                new FileInputStream(fileName))) {
-
-            while (true) {  //Reads untill EOF
-                readOne = (Production) inputStream.readObject();
-                readProductions.add(readOne);
-                //System.out.println(readOne);
-                //System.out.println();
-            }
-
-        } catch (EOFException eof) {
-            System.out.println("Reading Done! ");
-
-        } catch (IOException e) {
-            System.err.println("Error opening input file "
-                    + fileName + ": " + e.getMessage());
-            System.exit(0);
-        }
-        catch (ClassNotFoundException ex) {
-            System.err.println(ex);
-        }
-        return readProductions;
-    }
-
-
-    public static void main(String[] args) {
-/*
-        // tester læs & skriv
-        Production testProduktion = new Production("Badehotellet", "sæson1", new Date(5000));
-        Production testProduktion2 = new Production("Tilbage til fremtiden", "del 3", new Date(100000));
-
-        // opretter arrays med produktioner
-        List<Production> production = new ArrayList<>();
-        production.add(testProduktion);
-        production.add(testProduktion2);
-
-        // Gemmer produktion
-        DataFacade saveData = new DataFacade();
-        saveData.saveProduction(testProduktion);
-        saveData.saveProduction(testProduktion2);
-
-        // læser produktion
-        DataFacade getData = new DataFacade();
-        production = getData.getProductions();
-        System.out.println(production);
-
-
-    }
-
- */
 }
