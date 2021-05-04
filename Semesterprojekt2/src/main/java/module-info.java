ff26e49fd773c10f45ea31005161bb0bcb9b7cb7
Mette Hansen <meha419@student.sdu.dk>
2021-05-04 09:35:10 +0200

    Maven - again

--- Semesterprojekt2/src/main/java/module-info.java
+++ Semesterprojekt2/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module org.example {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires org.postgresql.jdbc;
 
     opens org.presentation to javafx.fxml;
     opens domain to javafx.base;
