79e8ce4bc48a14d62a3382ffce88a5ca48fac83a
Mette Hansen <meha419@student.sdu.dk>
2021-05-18 10:41:44 +0200

    Start test + maven update

---
+++ Semesterprojekt2/src/test/java/data/DataFacadeTest.java
@@ -0,0 +1,118 @@
+package data;
+
+import static org.junit.Assert.*;
+public class DataFacadeTest {
+
+    @org.junit.Test
+    public void getInstance() {
+        System.out.println("hi");
+    }
+
+    @org.junit.Test
+    public void createProduction() {
+    }
+
+    @org.junit.Test
+    public void getProductions() {
+    }
+
+    @org.junit.Test
+    public void getProduction() {
+    }
+
+    @org.junit.Test
+    public void deleteProduction() {
+    }
+
+    @org.junit.Test
+    public void updateProduction() {
+    }
+
+    @org.junit.Test
+    public void createCredits() {
+    }
+
+    @org.junit.Test
+    public void getCredits() {
+    }
+
+    @org.junit.Test
+    public void getCredit() {
+    }
+
+    @org.junit.Test
+    public void deleteCredit() {
+    }
+
+    @org.junit.Test
+    public void updateCredit() {
+    }
+
+    @org.junit.Test
+    public void createCreditName() {
+    }
+
+    @org.junit.Test
+    public void getCreditNames() {
+    }
+
+    @org.junit.Test
+    public void getCreditName() {
+    }
+
+    @org.junit.Test
+    public void deleteCreditName() {
+    }
+
+    @org.junit.Test
+    public void updateCreditName() {
+    }
+
+    @org.junit.Test
+    public void createProductionName() {
+    }
+
+    @org.junit.Test
+    public void getGenres() {
+    }
+
+    @org.junit.Test
+    public void validateProduction() {
+    }
+
+    @org.junit.Test
+    public void invalidateProduction() {
+    }
+
+    @org.junit.Test
+    public void validateCredit() {
+    }
+
+    @org.junit.Test
+    public void invalidateCredit() {
+    }
+
+    @org.junit.Test
+    public void dateFormatter() {
+    }
+
+    @org.junit.Test
+    public void getAllCreditTypes() {
+    }
+
+    @org.junit.Test
+    public void getAllProductionTypes() {
+    }
+
+    @org.junit.Test
+    public void getAllLanguages() {
+    }
+
+    @org.junit.Test
+    public void getAllGenres() {
+    }
+
+    @org.junit.Test
+    public void insertImage() {
+    }
+}
