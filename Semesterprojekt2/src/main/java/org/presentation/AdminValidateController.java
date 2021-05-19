42e3e33262fd607b6f96fa89acf2a810f13c2073
Karl-Emil Pantzar <kapan20@student.sdu.dk>
2021-05-19 10:13:23 +0200

    GUI Virker
    Ændret metoder, GUI virker

--- Semesterprojekt2/src/main/java/org/presentation/AdminValidateController.java
+++ Semesterprojekt2/src/main/java/org/presentation/AdminValidateController.java
@@ -1,9 +1,23 @@
 package org.presentation;
 
 import java.io.IOException;
+import java.util.Date;
+
+import domain.Production;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class AdminValidateController {
+
+    @FXML
+    TableView validationTableView;
+
+    @FXML
+    Button adminLandingButton, searchProductionsButton, invalidateButton, selectNoneButton, approveButton, validateButton, selectAllButton, changeProductionButton, changeCreditButton, addProductionButton, addCreditButton;
 
     @FXML
     private void switchToPrimary() throws IOException {
@@ -18,5 +32,62 @@ public class AdminValidateController {
     @FXML
     private void switchToAdminLanding() throws IOException {
         App.setRoot("administration");
+    }
+
+    public void switchToProductions(ActionEvent actionEvent) throws IOException {
+        App.setRoot("search");
+    }
+
+    private void setTableViewProduction(){
+
+//        private ArrayList<String> genre missing
+
+
+        validationTableView.getColumns().clear();
+        validationTableView.getItems().clear();
+
+        //creates a new column in the TableView with header "ID", type Production and cellValue String
+        TableColumn<Production, String> col1 = new TableColumn<>("Produktions ID");
+
+        //deciding what values go in the cells. Here it calls production.getId() to find value for the cell
+        col1.setCellValueFactory(new PropertyValueFactory<>("id"));
+
+        TableColumn<Production, String> col2 = new TableColumn<>("Title");
+        col2.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        TableColumn<Production, Date> col3 = new TableColumn<>("Release Date");
+        col3.setCellValueFactory(new PropertyValueFactory<>("releaseDate"));
+
+        TableColumn<Production, String> col4 = new TableColumn<>("Type");
+        col4.setCellValueFactory(new PropertyValueFactory<>("productionType"));
+
+        TableColumn<Production, String> col5 = new TableColumn<>("Episode");
+        col5.setCellValueFactory(new PropertyValueFactory<>("episode"));
+
+        TableColumn<Production, String> col6 = new TableColumn<>("Season");
+        col6.setCellValueFactory(new PropertyValueFactory<>("season"));
+
+        TableColumn<Production, String> col7 = new TableColumn<>("Length");
+        col7.setCellValueFactory(new PropertyValueFactory<>("length"));
+
+        TableColumn<Production, String> col8 = new TableColumn<>("Language");
+        col8.setCellValueFactory(new PropertyValueFactory<>("language"));
+
+        TableColumn<Production, String> col9 = new TableColumn<>("Company");
+        col9.setCellValueFactory(new PropertyValueFactory<>("companyProductionName"));
+
+        //adding columns to the tableview
+
+        validationTableView.getColumns().add(col1);
+        validationTableView.getColumns().add(col2);
+        validationTableView.getColumns().add(col3);
+        validationTableView.getColumns().add(col4);
+        validationTableView.getColumns().add(col5);
+        validationTableView.getColumns().add(col6);
+        validationTableView.getColumns().add(col7);
+        validationTableView.getColumns().add(col8);
+        validationTableView.getColumns().add(col9);
+
+
     }
 }
--- Semesterprojekt2/src/main/java/org/presentation/AdministrationController.java
+++ Semesterprojekt2/src/main/java/org/presentation/AdministrationController.java
@@ -1,6 +1,8 @@
 package org.presentation;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class AdministrationController {
@@ -18,5 +20,9 @@ public class AdministrationController {
     @FXML
     private void switchToAdminLanding() throws IOException {
         App.setRoot("administration");
+    }
+
+    public void switchToProductions(ActionEvent actionEvent) throws IOException {
+        App.setRoot("search");
     }
 }
