78cc1867679607e97f8ef6cf7a74cb7b04a2e88e
Mette Hansen <meha419@student.sdu.dk>
2021-05-21 14:40:20 +0200

    Cleanup Credit Class

--- Semesterprojekt2/src/main/java/domain/Credit.java
+++ Semesterprojekt2/src/main/java/domain/Credit.java
@@ -9,6 +9,9 @@ public class Credit {
     private int productionId;
 
     // Constructors
+    public Credit() {
+    }
+
     public Credit(CreditName creditName, String role, String creditType, boolean isValidated, int productionId) {
         this.creditName = creditName;
         this.role = role;
@@ -17,14 +20,10 @@ public class Credit {
         this.productionId = productionId;
     }
 
-    public Credit() {
-    }
-
-    // Methods
+    // Getters / Setters Methods
     public CreditName getCreditName() {
         return creditName;
     }
-
     public void setCreditName(CreditName creditName) {
         this.creditName = creditName;
     }
@@ -32,7 +31,6 @@ public class Credit {
     public String getRole() {
         return role;
     }
-
     public void setRole(String role) {
         this.role = role;
     }
@@ -40,7 +38,6 @@ public class Credit {
     public String getCreditType() {
         return this.creditType;
     }
-
     public void setCreditType(String creditType) {
         this.creditType = creditType;
     }
@@ -48,68 +45,34 @@ public class Credit {
     public boolean isValidated() {
         return isValidated;
     }
-
     public void setValidated(boolean validated) {
         isValidated = validated;
-    }
-
-    public String getFirstName(){
-        return this.creditName.getFirstName();
-    }
-
-    public String getLastName(){
-        return this.creditName.getLastName();
-    }
-
-    public String toString(){
-        return ("\n" + this.creditType + "\nrolle: " + this.role + "\nNavn: " + this.creditName.toString() + "\n");
-    }
-
-    public String getAddress() {
-        return this.creditName.getAddress();
-    }
-
-    public String getEmail() {
-        return this.creditName.getEmail();
-    }
-
-    public int getPhone() {
-        return this.creditName.getPhone();
-    }
-
-    public void setAddress(String address) {
-        this.creditName.setAddress(address);
-    }
-
-    public void setPhone(int phone) {
-       this.creditName.setPhone(phone);
-    }
-
-    public void setEmail(String email) {
-        this.creditName.setEmail(email);
     }
 
     public int getProductionId() {
         return productionId;
     }
-
     public void setProductionId(int productionId) {
         this.productionId = productionId;
+    }
+
+    // Methods that calls creditName
+    public String getFirstName(){
+        return this.creditName.getFirstName();
+    }
+    public String getLastName(){
+        return this.creditName.getLastName();
     }
 
     public String getImageUrl() {
         return this.creditName.getImageUrl();
     }
-
     public void setImageUrl(String imageUrl) {
         this.creditName.setImageUrl(imageUrl);
     }
 
-    public void setFirstName(String firstName){
-        this.creditName.setFirstName(firstName);
-    }
-
-    public void setLastName(String lastName){
-        this.creditName.setLastName(lastName);
+    //toString method
+    public String toString(){
+        return ("\n" + this.creditType + "\nrolle: " + this.role + "\nNavn: " + this.creditName.toString() + "\n");
     }
 }
